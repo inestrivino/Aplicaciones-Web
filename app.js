1b352ba317356866857881f2a39eaad8a09a5c6a
Inés Triviño <115464170+inestrivino@users.noreply.github.com>
2025-10-23 16:46:43 +0200

    Server update

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// servidor
+const http = require('http');
+let servidor = http.createServer(function(request,response){});
+servidor.listen(3000, function(error){
+    if(error)
+        console.log('error');
+    else
+        console.log('Servidos en 3000');
+});
